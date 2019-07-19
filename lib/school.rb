b92ca226d03f1225b3330fe98f38e5568e5de91a
Audrea Cook <audreacwork@gmail.com>
2019-07-19 14:06:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    
+  end
+  
+  def roster
+    roster = {}
+  end
+  
+  def add_student(name, grade)
+    if roster.has_key?(grade)
+      roster[:grade] << name
+    else
+      roster[:grade] = [name]
+     
+    end
+    roster
+  end
+end
+
