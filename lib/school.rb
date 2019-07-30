99c2f1d598b633fee88999cd753f78d835dd7957
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-30 20:39:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_reader :roster
+  def initialize(school_name)
+    @school = school_name
+    @roster = {}
+  end
+  def add_student(name,grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    roster.each do |grade,names|
+      names.sort!
+    end
+    roster
+  end
+end
